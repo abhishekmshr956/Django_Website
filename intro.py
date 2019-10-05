13bd4ed7e8029941058604d1b8f1af7bdf728deb
Abhishek Mishra <abhishekmshr956@gmail.com>
2019-10-05 22:21:29 +0200

    Making a website using Django

---
+++ intro.py
@@ -0,0 +1,24 @@
+
+# Object Oriented Programming Python
+
+
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('John', 'Smith')
+
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname())
+
+
