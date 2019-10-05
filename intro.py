5d18584b4bc51b1e67a80982cdb1c48f484da696
Abhishek Mishra <abhishekmshr956@gmail.com>
2019-10-05 22:38:34 +0200

    Testing git push

--- intro.py
+++ intro.py
@@ -4,7 +4,7 @@
 
 class Employee:
 
-    raise_amt = 1.04
+    raise_amt = 1.05
 
     def __init__(self, first, last):
         self.first = first
@@ -14,11 +14,10 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+
 emp_1 = Employee('John', 'Smith')
 
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname())
-
-
